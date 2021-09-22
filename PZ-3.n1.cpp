cd92104da9e7eaff6cf64d5c936ac0417479ea36
N1kiT0S <canngabach5869@gmail.com>
2021-09-22 11:47:21 +0300

    Create PZ-3.n1.cpp

---
+++ PZ-3.n1.cpp
@@ -0,0 +1,26 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using  namespace std;
+
+int main()
+{
+	double a, b, S, P;
+	cout << "a= ";  cin >> a;
+	cout << "b= ";  cin >> b;
+	S = a * b;
+	P = 2 * (a + b);
+	cout << "S = "<< S << endl;
+	cout << "P = " << P << endl;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			cout<< "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+
+}
+
